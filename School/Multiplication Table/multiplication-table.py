f7d972123b5ebc8b8baa92de00c22d8bc4d9ebcd
Nakula Gowda <91659838+nakulamk@users.noreply.github.com>
2023-03-09 01:04:44 +0530

    Added Solution - GfG to GitHub

---
+++ School/Multiplication Table/multiplication-table.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+class Solution:
+    def getTable(self,N):
+        # code here
+        arr=[]
+        for i in range(1,11,+1):
+            val=N*i
+            arr.append(val)
+        return arr
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ans = ob.getTable(N)
+        for i in ans:
+            print(i,end=" ")
+        print()
+# } Driver Code Ends
