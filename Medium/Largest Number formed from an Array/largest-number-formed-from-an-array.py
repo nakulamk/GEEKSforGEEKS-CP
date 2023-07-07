bcba6c3273c3dc1484234fc055d3671ca4b22f0e
Nakula Gowda <91659838+nakulamk@users.noreply.github.com>
2023-07-07 15:29:32 +0530

    Update largest-number-formed-from-an-array.py

--- Medium/Largest Number formed from an Array/largest-number-formed-from-an-array.py
+++ Medium/Largest Number formed from an Array/largest-number-formed-from-an-array.py
@@ -15,8 +15,26 @@ class Solution:
     #     arr=sorted(arr,key=cmp_to_key(compare))
     #     return str(int("".join(arr)))
     
+	# def printLargest(self,arr):
+ #        return "".join(sorted(arr,reverse=True,key=lambda _:_*18))   
 	def printLargest(self,arr):
-        return "".join(sorted(arr,reverse=True,key=lambda _:_*18))   
+        for i,n in enumerate(arr):
+            arr[i]=str(n)
+        if len(arr)<2:
+            return "".join(arr)
+        def compare(x,y):
+            return int(arr[x]+arr[y])> int(arr[y]+arr[x])
+        
+        for i in range(len(arr)-1):
+            j=i+1
+            while i<len(arr) and j<len(arr):
+                if compare(i,j):
+                    pass
+                else:
+                    arr[j],arr[i]=arr[i],arr[j]
+                    # swaping 
+                j+=1
+        return str(int("".join(arr)))
 
 
 #{ 
