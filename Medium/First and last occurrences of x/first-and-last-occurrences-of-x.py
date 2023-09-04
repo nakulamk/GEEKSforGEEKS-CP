d05a5765a73de0c4f3e97b241724518d70971065
Nakula Gowda <91659838+nakulamk@users.noreply.github.com>
2023-09-04 21:16:52 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/First and last occurrences of x/first-and-last-occurrences-of-x.py
@@ -0,0 +1,29 @@
+#User function Template for python3
+
+
+def find(arr,n,x):
+    # code here
+    l=[]
+    for i in range(n):
+        if arr[i]==x:
+            l.append(i)
+    if len(l)>0:
+        return [l[0],l[-1]]
+    else:
+        return [-1,-1]
+
+
+
+
+
+#{ 
+ # Driver Code Starts
+t=int(input())
+for _ in range(0,t):
+    l=list(map(int,input().split()))
+    n=l[0]
+    x=l[1]
+    arr=list(map(int,input().split()))
+    ans=find(arr,n,x)
+    print(*ans)
+# } Driver Code Ends
