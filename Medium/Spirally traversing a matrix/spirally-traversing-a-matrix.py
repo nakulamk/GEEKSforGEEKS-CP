13f3bc01b5b98ed06c8af26709c4e0d91a8f280f
Nakula Gowda <91659838+nakulamk@users.noreply.github.com>
2023-09-14 21:10:44 +0530

    Updated Solution - GfG to GitHub

--- Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.py
+++ Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.py
@@ -2,39 +2,7 @@
 
 class Solution:
     
-    #Function to return a list of integers denoting spiral traversal of matrix.
-    # def spirallyTraverse(self,matrix, r, c): 
-        # code here 
-        # left=0
-        # right=len(matrix[0])
-        # top=0
-        # bottom=len(matrix)
-        # res=[]
-        # while left<right and top<bottom:
-        #     for i in range(left,right):
-        #         res.append(matrix[top][i])
-        #     top+=1
-            
-        #     for i in range(top,bottom):
-        #         res.append(matrix[i][right-1])
-        #     right-=1
-            
-        #     if not (left<right and top<bottom):
-        #         break
-            
-        #     for i in range(right-1,left-1,-1):
-        #         # left-1 beacuse we need to go till the left that is like <=
-        #         res.append(matrix[bottom-1][i])
-        #     bottom-=1
-            
-        #     for i in range(bottom-1,top-1):
-        #         # top-1 beacuse we need to go till top ie <= in python its like this
-        #         res.append(matrix[i][left])
-        #     left+=1
-            
-        
-        # return res
-    def spirallyTraverse(self,matrix, r, c):
+    def spirallyTraverse(self,matrix,r,c):
         top=0
         left=0
         right=c-1
@@ -43,18 +11,61 @@ class Solution:
         while top<=bottom and left<=right:
             for i in range(left,right+1):
                 res.append(matrix[top][i])
+                
             top+=1
+            
             for i in range(top,bottom+1):
                 res.append(matrix[i][right])
             right-=1
+            
+            
             if top<=bottom and left<=right:
                 for i in range(right,left-1,-1):
+                    
                     res.append(matrix[bottom][i])
                 bottom-=1
+            
                 for i in range(bottom,top-1,-1):
+                    
                     res.append(matrix[i][left])
                 left+=1
-        return res
+        
+        return res   
+    
+    
+    
+    
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+
+
+
 
 
 #{ 
