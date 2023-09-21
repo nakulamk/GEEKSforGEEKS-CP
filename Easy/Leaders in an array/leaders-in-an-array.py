91223979980db1e68847164f1970ca5369b4e27c
Nakula Gowda <91659838+nakulamk@users.noreply.github.com>
2023-09-21 08:50:06 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Leaders in an array/leaders-in-an-array.py
@@ -0,0 +1,48 @@
+class Solution:
+    #Back-end complete function Template for Python 3
+    
+    #Function to find the leaders in the array.
+    def leaders(self, A, N):
+        #Code here
+        res=[]
+        max_right=0
+        for i in range(N-1,-1,-1):
+            if A[i]>=max_right:
+                res.append(A[i])
+                max_right=A[i]
+        
+        res.reverse()
+        return res
+            
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+    
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        N=int(input())
+        
+        A=[int(x) for x in input().strip().split()]
+        obj = Solution()
+        
+        A=obj.leaders(A,N)
+        
+        for i in A:
+            print(i,end=" ")
+        print()
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
