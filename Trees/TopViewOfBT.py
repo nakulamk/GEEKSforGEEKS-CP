a0ccc5ddf8261100832be1dc914c8caf0fde0f23
Nakula Gowda <91659838+nakulamk@users.noreply.github.com>
2023-11-03 00:37:09 +0530

    Create TopViewOfBT.py

---
+++ Trees/TopViewOfBT.py
@@ -0,0 +1,29 @@
+class Solution:
+    
+    #Function to return a list of nodes visible from the top view 
+    #from left to right in Binary Tree.
+    def topView(self,root):
+        
+        # code here
+        ans=[]
+        if root is None:
+            return ans
+        mpp={}
+        q=deque()
+        q.append((root,0))
+        
+        while q:
+            node,line=q.popleft()
+            if line not in mpp:
+                mpp[line]=node.data
+            
+            if node.left:
+                q.append((node.left,line-1))
+            if node.right:
+                q.append((node.right,line+1))
+        
+        
+        for line in sorted(mpp):
+            ans.append(mpp[line])
+        
+        return ans
